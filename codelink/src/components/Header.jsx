9880d10a9191649ba82056eeff9c058d9a8186cc
Iris <irisreinoso74@gmail.com>
2025-10-30 15:41:29 -0300

    Agregado de botones de navegación

--- codelink/src/components/Header.jsx
+++ codelink/src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="encabezado">
       <div className="logo">
-        <img src="/logo.jpg" alt="Logo Empresa" className="logo-img" />
+        <img src="coDELINK.jpg" alt="Logo Empresa" className="logo-img" />
         <h1>Red de Emprendimientos</h1>
       </div>
       <nav className="menu">
